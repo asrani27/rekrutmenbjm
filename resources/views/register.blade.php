eb4568b2e2c7558cc7c32b948876ca6c2e54d572
asrani <asrani.27@gmail.com>
2024-09-24 09:18:00 +0800

    f

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -36,7 +36,7 @@
       <div class="col-12 col-lg-6 col-xl-4 border-top-wide border-primary d-flex flex-column justify-content-center">
         <div class="container container-tight my-5 px-lg-5">
           <div class="text-center mb-4">
-            <a href="." class="navbar-brand navbar-brand-autodark"><img src="/icon/bjmmemanggil.png"  alt=""></a>
+            <a href="." class="navbar-brand navbar-brand-autodark"><img src="/icon/logobjm.png"  alt=""></a>
           </div>
           <h2 class="h3 text-center mb-3">
             Create new free Account
@@ -105,7 +105,7 @@
       </div>
       <div class="col-12 col-lg-6 col-xl-8 d-none d-lg-block">
         <!-- Photo -->
-        <div class="bg-cover h-100 min-vh-100" style="background-image: url('/icon/bg2.jpg')">
+        <div class="bg-cover h-100 min-vh-100" style="background-image: url('/icon/tm2.jpg')">
         </div>
       </div>
     </div>
