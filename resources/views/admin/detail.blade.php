5fd060f5a4710a35d362e201ecb226b37f971b16
asrani <asrani.27@gmail.com>
2024-10-15 15:03:30 +0800

    f

--- resources/views/admin/detail.blade.php
+++ resources/views/admin/detail.blade.php
@@ -198,13 +198,13 @@
         
 
         @if ($data->status_kirim == 1)
-          {{-- @if ($data->preview_ktp == null || $data->preview_ijazah == null || $data->preview_sertifikat == null)
+          @if ($data->preview_ktp == null || $data->preview_ijazah == null)
               
           <a href="#" class='btn btn-success disabled w-50' disabled> <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg> VALIDASI</a><br/><br/>
-          @else --}}
+          @else
               
           <a href="#" class='btn btn-success validasi w-50' data-id="{{$data->id}}"> <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg> VALIDASI</a><br/><br/>
-          {{-- @endif --}}
+          @endif
         @endif
         
     </div>
